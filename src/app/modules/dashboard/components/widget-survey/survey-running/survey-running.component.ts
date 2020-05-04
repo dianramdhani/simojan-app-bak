2509999c04bbc7322db0fb2d740da66a2bdef3d7
Dian Ramdhani Lubis <dian.ramdhani@gmail.com>
2020-05-04 15:05:53 +0700

    update survey-running: add surveyRunning

--- src/app/modules/dashboard/components/widget-survey/survey-running/survey-running.component.ts
+++ src/app/modules/dashboard/components/widget-survey/survey-running/survey-running.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit, Output, EventEmitter } from '@angular/core';
+import { Survey } from '@data/schema/survey';
+import { SurveyService } from '@data/services/survey.service';
+import { AlertController } from '@ionic/angular';
 
 @Component({
   selector: 'app-survey-running',
@@ -9,13 +12,34 @@ export class SurveyRunningComponent implements OnInit {
   @Output() switchSurvey = new EventEmitter<'history'>();
   eventRunning = false;
   mapOnline = false;
+  surveyRunning: Survey;
 
-  constructor() { }
+  constructor(
+    private surveyService: SurveyService,
+    private alertController: AlertController
+  ) { }
 
-  ngOnInit() { }
+  ngOnInit() {
+    this.surveyRunning = this.surveyService.surveyRunning;
+  }
 
-  stopSurvey() {
-    console.log('survey stopped');
-    this.switchSurvey.emit('history');
+  async stopSurvey() {
+    const alert = await this.alertController.create({
+      header: 'Survey stop?',
+      message: 'Are you sure you want to stop this survey?',
+      buttons: [
+        {
+          text: 'No',
+          role: 'no'
+        }, {
+          text: 'Yes',
+          handler: () => {
+            this.surveyService.stop();
+            this.switchSurvey.emit('history');
+          }
+        }
+      ]
+    });
+    await alert.present();
   }
 }
